025c8f640280c7aabd147087bb6acc5103cafacd
dmaupin <dm@davidmaupin.com>
2015-02-18 12:01:26 -0800

    added insta search

--- app/controllers/api/face_search_controller.rb
+++ app/controllers/api/face_search_controller.rb
@@ -2,7 +2,7 @@ class Api::FaceSearchController < ApplicationController
 
   def index
     image_url = "http://faceplusplus.com/static/img/demo/1.jpg"
-    @facedata = HTTParty.get("https://apius.faceplusplus.com/v2/detection/detect?url=#{image_url}&api_secret=#{ENV['FACE_SECRET']}&api_key=#{ENV['FACE_KEY']}&attribute=glass,pose,gender,age,race,smiling")
+    @facedata = HTTParty.get("https://apius.faceplusplus.com/v2/detection/detect?url=#{image_url}&api_secret=#{ENV['nipMH0pxm5XCz3mi7XYuw1f4mBFjv8vT']}&api_key=#{ENV['ca2075ce565bca77531270ff40dcf5a8']}&attribute=glass,pose,gender,age,race,smiling")
     render json: @facedata
   end
 
---
+++ app/models/instamodel.rb
@@ -0,0 +1,14 @@
+class Instamodel
+
+	def self.search
+		@resp = HTTParty.get("https://api.instagram.com/v1/media/search?lat=34.101644&lng=-118.339072&access_token=")
+		@url_list = []
+		@list = @resp['data'].each do |grams|
+			@url_list << grams['images']['standard_resolution']['url']
+		end
+
+		return @url_list
+	
+	end
+
+end
